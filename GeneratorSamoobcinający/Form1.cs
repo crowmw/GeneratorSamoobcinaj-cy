78497e5e2bf0115939edb97afcff97a6acaf4568
crowmw <crow.mw@gmail.com>
2014-11-27 15:52:25 +0100

    FirstPush

---
+++ GeneratorSamoobcinający/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeneratorSamoobcinający
+{
+    public partial class Form1 : Form
+    {
+        private StringBuilder outputCiag;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string seed = textBox1.Text;
+            LFSR lsfr = new LFSR(seed.Length, seed);
+            outputCiag = new StringBuilder(textBox3.Text);
+
+            for (int i = 0; i <= Convert.ToInt32(textBox3.Text); i++)
+            {
+                gen(lsfr);
+            }
+
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            if (checkBox1.Checked)
+            {
+                SaveToTXT();
+            }
+
+            if (checkBox2.Checked)
+            {
+                SaveToBIN();
+            }
+
+            WriteToTextBox();
+        }
+
+        private void WriteToTextBox()
+        {
+            textBox2.Text = "";
+            textBox2.Text = outputCiag.ToString();
+        }
+
+        private void SaveToBIN()
+        {
+            FileStream fs = new FileStream("out.bin", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fs);
+            writer.Write(outputCiag);
+            writer.Close();
+            fs.Close();
+        }
+
+        private void SaveToTXT()
+        {
+            FileStream fs = new FileStream("out.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fs);
+            writer.Write(outputCiag);
+            writer.Close();
+            fs.Close();
+        }
+
+        private void gen(LFSR lsfr)
+        {
+            bool[] outp;
+            outp = lsfr.Output;
+            if (outp[0] == true)
+            {
+                if (outp[1] == true)
+                    outputCiag.Append("1");
+                else
+                    outputCiag.Append("0");
+            }
+            else
+            {
+                lsfr.Shift();
+                this.gen(lsfr);
+            }
+            lsfr.Shift();
+        }
+    }
+}
---
+++ GeneratorSamoobcinający/LFSR.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneratorSamoobcinający
+{
+    class LFSR
+    {
+        bool[] bits;
+
+        public LFSR(int bitCount, string seed)
+        {
+            bits = new bool[bitCount];
+            for (int i = 0; i < bitCount; i++)
+                bits[i] = seed[i] == '1' ? true : false;
+        }
+
+        public string Registry
+        {
+            get
+            {
+                char[] tmp = new char[bits.Length];
+                for (int i = 0; i < bits.Length; i++)
+                    tmp[i] = bits[i] ? '1' : '0';
+                return new string(tmp);
+            }
+        }
+
+        public bool[] Output
+        {
+            get
+            {
+                return new bool[] { bits[0], bits[1] };
+            }
+        }
+
+        public void Shift()
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                bool bnew = !(bits[bits.Length - 1] == bits[bits.Length - 2]);
+
+                for (int i = bits.Length - 1; i > 0; i--)
+                {
+                    bits[i] = bits[i - 1];
+                }
+                bits[0] = bnew;
+            }
+        }
+    }
+}
